d24ed3ead0934f90f52158e9a118eb2fc1251739
Aaron Watson <aaron@Aarons-MacBook.local>
2017-02-12 15:36:04 -0500

    parameratize everything and put the rest into functions!

--- main.py
+++ main.py
@@ -1,26 +1,62 @@
 import dropbox_download
 import pdf_extract
-from wordcloud import WordCloud
+from wordcloud import WordCloud, ImageColorGenerator
 from PIL import Image
 from nose.tools import assert_equal
 import argparse
+import numpy as np
+from os import path
 
 def prep_args():
     parser = argparse.ArgumentParser(description='Pull Consumer Affairs DigiTech PDFs from Dropbox and build a word cloud image.')
     parser.add_argument('-k', '--key', action='store', dest='api_key', type=str, help='Dropbox API key', required=True)
     parser.add_argument('-d', '--dimensions', action='store', dest='dimensions', type=str, default='800,600', help='Image dimensions (default: 800,600)')
     parser.add_argument('-f', '--font', action='store', dest='font', type=str, default='./Qraxy.ttf', help='Font family (default: Qraxy.ttf)')
+    parser.add_argument('-fs', '--fontsize', action='store', dest='max_font_size', type=int, default=300, help='Maximum font size used in the cloud.')
+    parser.add_argument('-m', '--mask', action='store', dest='mask', type=str, help='Image mask to use. Size will override dimensions parameters.')
+    parser.add_argument('-c', '--colorize', action='store_true', dest='colorize', help='Use colors in the mask in the resulting cloud image.')
+    parser.add_argument('-o', '--output', action='store', dest='filename', help='Destination filename for the cloud image.', required=True)
+    parser.add_argument('-w', '--words', action='store', dest='max_words', type=int, default=125, help='Maximum number of words in the cloud.')
     return parser.parse_args()
 
-def write_cloud(args, text, filename):
-    wc = WordCloud()
-    dimensions = args.dimensions.split(',')
+def apply_dimensions(wc, dimensions):
+    arr_dimensions = dimensions.split(',')
 
-    if all(dimension.isdigit() for dimension in dimensions):
-        wc.width = int(dimensions[0])
-        wc.height = int(dimensions[1])
+    if all(dimension.isdigit() for dimension in arr_dimensions):
+        wc.width = int(arr_dimensions[0])
+        wc.height = int(arr_dimensions[1])
+    else:
+        wc.width = 800
+        wc.height = 600
+
+def apply_mask(wc, mask_file):
+    mask = np.array(Image.open(mask_file))
+    wc.mask = mask
+    wc.width = mask.shape[1]
+    wc.height = mask.shape[0]
+    return mask
+
+def apply_colors(wc, mask):
+    mask_colors = ImageColorGenerator(mask)
+    wc.recolor(color_func=mask_colors)
+
+def generate_cloud(args, text, filename):
+    wc = WordCloud()
+
+    apply_dimensions(wc, args.dimensions)
+
     wc.font_path = args.font
+    wc.max_words = args.max_words
+
+    if args.mask is not None:
+        mask = apply_mask(wc, args.mask)
+
+    wc.max_font_size = args.max_font_size
+
     wc.generate(text)
+
+    if args.mask is not None and args.colorize:
+        apply_colors(wc, mask)
 
     wc.to_file(filename)
     loaded_image = Image.open(filename)
@@ -41,7 +77,7 @@ def main(args):
 
     cloud_text = pdf_extract.combine_similar(cloud_text)
 
-    write_cloud(args, cloud_text, 'cloud.png')
+    generate_cloud(args, cloud_text, args.filename)
 
 args = prep_args()
 main(args)
